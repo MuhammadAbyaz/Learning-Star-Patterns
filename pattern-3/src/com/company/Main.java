2d27137a7b776c473d117908ec51064dfe3c5ed0
MuhammadAbyaz <abyazkhalid2005@gmail.com>
2021-05-18 23:38:12 +0500

    Completed Pattern 3,4

---
+++ pattern-3/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for(int i = 1; i<=5; i++){
+            for(int j = 1; j<= 5; j++){
+                if(j>= i){
+                    System.out.print("*");
+                }else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
+    }
+}
