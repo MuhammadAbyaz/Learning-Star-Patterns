680493225de458ea8ca9270e4de96cc7c049c8ba
MuhammadAbyaz <abyazkhalid2005@gmail.com>
2021-07-31 20:34:20 +0500

    completed pattern 17

---
+++ pattern-17/src/com/company/Main.java
@@ -0,0 +1,24 @@
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int k = 0;
+        for(int i = 1; i<=9; i++){
+            if(i<=5){
+                k++;
+            }else {
+                k--;
+            }
+            for(int j = 1; j<= 9; j++){
+                if(j<=6-k || j>=4+k){
+                    System.out.print("*");
+                }else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
+    }
+}
